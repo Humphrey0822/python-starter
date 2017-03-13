41b74f3a7411c72284b1f49c2220614fad8bf341
Humphrey Sun <sunzhiqiang@ouyeel.com>
2017-03-13 22:47:45 +0800

    去掉不必要的分号

--- execute_time/excute_time_test3.py
+++ execute_time/excute_time_test3.py
@@ -4,7 +4,7 @@ import datetime
 
 def time_1():
     begin = datetime.datetime.now()
-    sum = 0;
+    sum = 0
     for i in xrange(10000000):
         sum = sum + i
     end = datetime.datetime.now()
