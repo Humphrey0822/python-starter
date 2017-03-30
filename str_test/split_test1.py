74d364c9b467335185849142cb16e14dba191115
Humphrey <sunzhiqiang@ouyeel.com>
2017-03-30 15:37:22 +0800

    使用split(char c)：使用特定字符分割字符串成字符数组

--- str_test/split_test1.py
+++ str_test/split_test1.py
@@ -13,5 +13,20 @@ def split_print(text):
 
 text = "年老代的内存使用率 all(#3) old.gen.mem.ratio jmxport=8999 26.93>90"
 text1 = "购物车模块10.70.14.72:8080连接时长（大于500ms,且持续3分钟警告） all(#3) cart_ng.hc.timing/server=10.70.14.72_8080.col  40431>500"
+text2 = "磁盘读速度 /秒 超过1000且持续时间超过10分钟报警 all(#10) disk.io.msec_read device=sdb 945.35>1000"
+# split_print(text2)
+# print "============="
+# split_print(text1)
 
-split_print(text1)
+
+def get_full_metric(msg):
+    s = msg.split(" ")
+    n = len(s)
+    if s[n-2] == '':
+        full_metric = s[n-3]
+    else:
+        full_metric = s[n-3] + '/' + s[n-2]
+    return full_metric.strip()
+
+
+print get_full_metric(text1)
